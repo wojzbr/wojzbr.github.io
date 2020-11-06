7645af7e997a2daaffe22f2b604cd002227ada42
Wojtek <wojciech.zbrozek@gmail.com>
2020-11-06 15:50:37 +0100

    Updates

---
+++ precache-manifest.707c1ff2ee8f919b3b806d51da677f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a579233c99093fa42125e2b2a44b5fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1966440eaef6d9578e",
+    "url": "/static/css/main.85336000.chunk.css"
+  },
+  {
+    "revision": "da83fb3c1130d0d89ee0",
+    "url": "/static/js/2.78882c00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78882c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1966440eaef6d9578e",
+    "url": "/static/js/main.0c13b2f5.chunk.js"
+  },
+  {
+    "revision": "588db7dfeed336298693",
+    "url": "/static/js/runtime-main.260c0458.js"
+  }
+]);
